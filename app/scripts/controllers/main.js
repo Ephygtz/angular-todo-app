85309300c62c4dc44948551ab57aabcc94de9c39
Ephraim <ephygtz@gmail.com>
2015-12-03 16:52:40 +0300

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularToDoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularToDoApp
+ */
+angular.module('angularToDoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.todos = ['Item 1', 'Item 2', 'Item 3', 'item 4'];
+    $scope.addToDo = function() {
+        // To work on..
+      // if ($scope.textArea === " ") {
+      //   alert('You cannot add an empty to do');
+      //   return false;
+      // } else {
+        $scope.todos.push($scope.todo);
+        $scope.todo = '';
+      // }
+
+    }
+  });
